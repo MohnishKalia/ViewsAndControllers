24471def15c5e6b3c5568ef72a159ad169fd11c3
MohnishKalia <20kaliam@elmbrookstudents.org>
2019-06-19 11:50:49 -0500

    Date

---
+++ ViewsAndControllers/ViewsAndControllers/Controllers/DateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class DateController : Controller
+    {
+        // GET: Date
+         public ActionResult Index()
+        {
+            return View(viewName: "Index", model: DateTime.Now.ToString("d"));
+        }
+        [HttpPost]
+        public ActionResult DateData(DateTime inputDate)
+        {
+            return View(viewName: "Index", model: inputDate.ToString("d"));
+        }
+    }
+}
---
+++ ViewsAndControllers/ViewsAndControllers/Controllers/ExtensionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewsAndControllers.Controllers
+{
+    public class ExtensionController : Controller
+    {
+        // GET: Extension
+        public ActionResult Index()
+        {
+            return View(viewName: "Index");
+        }
+        public ActionResult 
+    }
+}
